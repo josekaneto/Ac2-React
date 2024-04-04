6a3e7c987d14c089bcbf26cbc32e28108ba42cb1
guilherme sena <iugprofissional@gmail.com>
2024-04-04 19:44:33 -0300

    apresentacao

---
+++ src/Componentes/Apresentacao/Apresentacao.jsx
@@ -0,0 +1,12 @@
+function Apresentacao(){
+    return(
+        <div id="apresentacao">
+            <img src="public/Screen Shot 2024-03-09 at 7.04.34 PM.png" alt="" />
+            <div id="texto_apresentacao">
+                <p>Olá! Sou José Otavio Kaneto, um entusiasta da programação dedicado a criar soluções inovadoras. Com habilidades em HTML5, CSS3, JavaScript, MongoDB, SQL e Node.js, encontro motivação na resolução de desafios e na busca contínua por conhecimento. Além de ser apaixonado por programação, passo horas me entretendo com projetos pessoais e explorando novas ferramentas. Meu portfólio reflete não apenas minhas habilidades técnicas, mas também minha paixão e comprometimento com a área. Estou ansioso para contribuir com projetos empolgantes no futuro</p>
+            </div>
+        </div>
+    )
+}
+
+export default Apresentacao
